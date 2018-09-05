bdad9fcf2dbd333d88ee02a4d73f39f74378c270
Arjun Shome <arjunshome111@gmail.com>
2018-09-05 17:41:21 +0530

    Updated Stored Procedure
    Updated Stored Procedures for Setting No COUNT ON and Update LoginTime

--- Code/SQL/TableScripts/USP_DU_Tenant_Login.sql
+++ Code/SQL/TableScripts/USP_DU_Tenant_Login.sql
@@ -3,14 +3,27 @@ Author:- Arjun Shome
 Description :- Login Stored procedure Script for Divya Urbania
 */
 GO
-CREATE PROCEDURE USP_DU_Tenant_Login
+ALTER PROCEDURE USP_DU_Tenant_Login
 @id nvarchar(50), @PASSWD nvarchar(50), 
 @message NVARCHAR(100) OUT
 AS
 BEGIN
 	IF EXISTS(SELECT * FROM dbo.DU_Login WHERE LoginEmail = @id AND LoginPassword = @PASSWD AND Active = 1)
 	BEGIN
-		SELECT @message = 'Successfully Logged IN'
+		BEGIN TRANSACTION
+
+		UPDATE LG  SET LG.LastLoginTime = GETDATE()
+		FROM dbo.DU_Login LG 
+		WHERE LG.LoginEmail = @id
+			AND LG.LoginPassword = @PASSWD
+
+		IF @@ROWCOUNT = 1 AND @@ERROR = 0
+		BEGIN 
+			COMMIT
+			SELECT @message = 'Successfully Logged IN'
+		END
+		ELSE
+		ROLLBACK
 	END
 	ELSE
 	BEGIN
@@ -27,5 +40,5 @@ END
 GO
 
 DECLARE @message NVARCHAR(100)
-EXECUTE USP_DU_Tenant_Login 'arjunshomtyte111@gmail.com','Arjunrama@829080',@message output
+EXECUTE USP_DU_Tenant_Login 'arjunshome111@gmail.com','Arjunrama@829080',@message output
 PRINT @message
