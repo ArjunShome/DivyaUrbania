c01672e19f9a98f67a3d55696f6023b0ac9d22c0
Arjun Shome <arjunshome111@gmail.com>
2018-09-10 21:26:48 +0530

    Added Solution for Divya Urbania

---
+++ Code/Solution/Divya_Urbania/Divya_Urbania/ArjunDev/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!F:\MY LEARNING\Divya Urbania\Project\Divya_Urbania\Divya_Urbania\ArjunDev\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
